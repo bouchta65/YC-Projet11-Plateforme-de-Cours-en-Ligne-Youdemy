1cc2992c3823f11dc45d642e1752273b17deb9ed
bouchta65 <bouchtamohamed01@gmail.com>
2025-01-20 14:48:03 +0100

    Implement CourseVideo class with saveCourse, getCourseType, and getContenu methods for PDF course handling

---
+++ src/classes/courseVideo.php
@@ -0,0 +1,37 @@
+<?php 
+require_once "course.php";
+class CourseVideo extends Course {
+    protected string $videoUrl;
+
+    public function __construct(int $idCours, string $titre, string $description, string $videoUrl, string $type ,string $image, int $idCategory, int $idTeacher, string $date_creation,string $typeCourse) {
+        parent::__construct($idCours, $titre, $description, $image, $type, $idCategory, $idTeacher, $date_creation,$typeCourse);
+        $this->videoUrl = $videoUrl;
+    }
+
+    public function getVideoUrl(): string {
+        return $this->videoUrl;
+    }
+
+    public function saveCourse(PDO $conn): void {
+        $sql = "INSERT INTO cours (titre, description, contenu, type, image, idCategory, idTeacher, typeCours) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(1, $this->titre, PDO::PARAM_STR);
+        $stmt->bindValue(2, $this->description, PDO::PARAM_STR);
+        $stmt->bindValue(3, $this->videoUrl, PDO::PARAM_STR);
+        $stmt->bindValue(4, $this->type, PDO::PARAM_STR);
+        $stmt->bindValue(5, $this->image, PDO::PARAM_STR);
+        $stmt->bindValue(6, $this->idCategory, PDO::PARAM_INT);
+        $stmt->bindValue(7, $this->idTeacher, PDO::PARAM_INT);
+        $stmt->bindValue(8, $this->typeCourse, PDO::PARAM_STR);
+        $stmt->execute();
+    }
+
+    public function getCourseType(): string {
+        return "Video";
+    }
+    public function getContenu(): string {
+        return $this->videoUrl;
+    }
+}
+
+?>
