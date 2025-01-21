54f931d3271186239c4a94d7776346d1c164e9ee
bouchta65 <bouchtamohamed01@gmail.com>
2025-01-21 10:08:52 +0100

    - Updated the User class constructor to include new attributes: image, address, and phone.

--- src/classes/user.php
+++ src/classes/user.php
@@ -1,22 +1,29 @@
 <?php 
 include "../db/config.php";
+require_once "teacher.php";
+require_once "student.php";
+require_once "admin.php";
 abstract class User{
-    private int $idUser;
-    private string $username;
-    private string $email;
-    private string $password;
-    private string $role;
-    private bool $status;
+    protected int $idUser;
+    protected string $username;
+    protected string $email;
+    protected string $password;
+    protected string $role;
+    protected string $status;
+    protected string $image;
+    protected string $address;
+    protected string $phone;
 
-
-    public function __construct(PDO $conn,int $idUser,string $username,string $email,string $password,string $role,bool $status) {
+    public function __construct(int $idUser,string $username,string $email,string $password,string $role,String $status,string $image,string $address,String $phone) {
         $this->idUser = $idUser;
         $this->username = $username;
         $this->email = $email;
         $this->password = $password;
         $this->role = $role;
         $this->status = $status;
-        $this->conn = $conn;
+        $this->image = $image;
+        $this->address = $address;
+        $this->phone = $phone;
     }
 
     public function getIdUser(): int
@@ -44,9 +51,23 @@ abstract class User{
         return $this->role;
     }
 
-    public function getStatus(): bool
+    public function getStatus(): string
     {
         return $this->status;
+    }
+    public function getImage(): string
+    {
+        return $this->image;
+    }
+
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+
+    public function getPhone(): string
+    {
+        return $this->phone;
     }
 
     public function setUsername(string $username): void
@@ -69,12 +90,31 @@ abstract class User{
         $this->role = $role;
     }
 
-    public function setStatus(bool $status): void
+    public function setStatus(string $status): void
     {
         $this->status = $status;
     }
 
+    
+    public function setImage(string $image): void
+    {
+        $this->image = $image;
+    }
+
+    
+    public function setAddress(string $address): void
+    {
+        $this->address = $address;
+    }
+
+    
+    public function setPhone(string $phone): void
+    {
+        $this->phone = $phone;
+    }
+
     public static function login(PDO $conn ,string $email,string $password): void{
+        session_start();
         $sql = "SELECT * from user where email = ?";
         $stmt = $conn->prepare($sql);
         $stmt->bindValue(1, $email, PDO::PARAM_STR);
@@ -83,11 +123,13 @@ abstract class User{
         if($user){
             if(password_verify($password,$user['password'])){
                 if($user['role']=="Student"){
-                    $_SESSION['user'] = new Student($conn,$user['idUser'],$user['username'],$user['email'],$user['password'],$user['role'],$user['status']);
-                }else if($user['role']=="teacher"){
-                    $_SESSION['user'] = new Teacher($conn,$user['idUser'],$user['username'],$user['email'],$user['password'],$user['role'],$user['status']);
+                    $_SESSION['user'] = serialize(new Student($user['idUser'],$user['username'],$user['email'],$user['password'],$user['role'],$user['statut'],$user['image'],$user['address'],$user['phone']));
+                }else if($user['role']=="Teacher"){
+                    $_SESSION['user'] = serialize(new Teacher($user['idUser'],$user['username'],$user['email'],$user['password'],$user['role'],$user['statut'],$user['image'],$user['address'],$user['phone']));
+                    echo "good";
+                    header('Location: ../views/TeacherCourses.php'); 
                 }else{
-                    $_SESSION['user'] = new Admin($conn,$user['idUser'],$user['username'],$user['email'],$user['password'],$user['role'],$user['status']);
+                    $_SESSION['user'] = serialize(new Admin($user['idUser'],$user['username'],$user['email'],$user['password'],$user['role'],$user['statut']),$user['image'],$user['address'],$user['phone']);
 
                 }
             }else {
@@ -99,35 +141,51 @@ abstract class User{
 
     }
 
-    public static function registre(PDO $conn ,string $email,string $password,string $date): void{
+    public static function registre(PDO $conn, string $name, string $email, string $password, string $role,string $image,string $address, string $phone): void {
         $sql = "SELECT email FROM user WHERE email = ?";
         $stmt = $conn->prepare($sql);
         $stmt->bindValue(1, $email, PDO::PARAM_STR);
         $stmt->execute();
         $user = $stmt->fetch(PDO::FETCH_NUM);
-
+    
         if ($user) {
-            echo "<script>alert('Lemail est déjà utilisé.');</script>";
+            echo "<script>alert('The email is already in use.');</script>";
         } else {
             $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-
-            $sql = "INSERT INTO user (Full_Name_User, Email_User, Age_User, Role_User, Password_User) VALUES (?, ?, ?, 'user', ?)";
-            $stmt = $this->conn->prepare($sql);
-            $stmt->bindValue(1, $name, PDO::PARAM_STR); 
-            $stmt->bindValue(2, $email, PDO::PARAM_STR); 
-            $stmt->bindValue(3, $age, PDO::PARAM_INT); 
-            $stmt->bindValue(4, $hashedPassword, PDO::PARAM_STR); 
-                
-            if ($stmt->execute()) {
-                echo "<script>alert('Inscription réussie.');</script>";
+    
+            if ($role == "Student") {
+                $sql = "INSERT INTO user (username, email, password, role, statut,image,address,phone) VALUES (?, ?, ?, 'Student', 'Active',?,?,?)";
             } else {
-                echo "<script>alert('Erreur lors de linscription.');</script>";
+                $sql = "INSERT INTO user (username, email, password, role, statut,image,address,phone) VALUES (?, ?, ?, 'Teacher', 'Pending',?,?,?)";
             }
-
+    
+            $stmt = $conn->prepare($sql);
+            $stmt->bindValue(1, $name, PDO::PARAM_STR);
+            $stmt->bindValue(2, $email, PDO::PARAM_STR);
+            $stmt->bindValue(3, $hashedPassword, PDO::PARAM_STR); 
+            $stmt->bindValue(4, $image, PDO::PARAM_STR); 
+            $stmt->bindValue(5, $address, PDO::PARAM_STR); 
+            $stmt->bindValue(6, $phone, PDO::PARAM_STR); 
+            $stmt->execute();
+    
+            echo "<script>alert('Registration successful!');</script>";
+        }
     }
+
+    public function logout(): void{
+        session_start();
+        unset($_SESSION['user']);
+        session_unset();
+        session_destroy();
+        header("Location: ../../index.php");
+        exit();
+    }
+
+
     
 
-    }
+  
+    
 }
     
 
