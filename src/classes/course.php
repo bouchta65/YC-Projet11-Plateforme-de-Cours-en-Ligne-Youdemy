9ff964acbe4071c226c6e02d88a627a50b2b2344
bouchta65 <bouchtamohamed01@gmail.com>
2025-01-22 21:57:09 +0100

    Finalize the development of the class

--- src/classes/course.php
+++ src/classes/course.php
@@ -1,5 +1,6 @@
 <?php
-include "../db/config.php";
+require_once "coursePdf.php";
+require_once "courseVideo.php";
 abstract class Course {
     protected int $idCours;
     protected string $titre;
@@ -95,12 +96,41 @@ abstract class Course {
         return $courses;
     }
 
-    public function deleteCourse(PDO $conn){
+    public static function topCourses(PDO $conn): array {
+        $sql = "SELECT   c.*, t.username , COUNT(ci.idStudent) AS student_count FROM 
+        cours c LEFT JOIN courseinscription ci ON c.idCours = ci.idCours 
+        LEFT JOIN user s ON ci.idStudent = s.idUser
+        JOIN user t ON t.idUser = c.idTeacher
+        GROUP BY 
+        c.idCours
+        ORDER BY 
+        student_count DESC LIMIT 3";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $courses = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $courses;
+    }
+    
+
+    public function deleteCourse(PDO $conn): void{
         $sql = "DELETE from cours where idCours = ?";
         $stmt = $conn->prepare($sql);
         $stmt->bindValue(1,$this->idCours,PDO::PARAM_INT);
         $stmt->execute();
     }
+
+    public static function getCourseById(PDO $conn, int $courseId): ?array {
+        $sql = "SELECT * FROM cours WHERE idCours = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(1, $courseId, PDO::PARAM_INT);
+        $stmt->execute();
+    
+        $courseData = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+        return $courseData;
+    }
+    
+    
 
   
 
@@ -110,7 +140,6 @@ abstract class Course {
     abstract public function setUrl(string $filrUrl): void;
     abstract public function getContenu(): string;
 
-    
 
     
 }
